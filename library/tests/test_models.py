59460505d737751ed8d645559f2e9f9884713cce
PravEpsic <pv19yjd@eduvaud.ch>
2024-05-23 22:46:05 +0200

    Modifiction des test_ models et test_url pour avoir des tests utilisant des fixitures

--- library/tests/test_models.py
+++ library/tests/test_models.py
@@ -1,33 +1,26 @@
-from django.test import TestCase
+import pytest
 from library.models import Book
 
-class BookModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
-        Book.objects.create(author='Author Name', title='Book Title')
+@pytest.fixture
+def book(db):
+    return Book.objects.create(author='Author Name', title='Book Title')
 
-    def test_author_label(self):
-        book = Book.objects.get(id=1)
-        field_label = book._meta.get_field('author').verbose_name
-        self.assertEqual(field_label, 'author')  # Modifiez cette ligne
+def test_author_label(book):
+    field_label = book._meta.get_field('author').verbose_name
+    assert field_label == 'author'
 
-    def test_title_label(self):
-        book = Book.objects.get(id=1)
-        field_label = book._meta.get_field('title').verbose_name
-        self.assertEqual(field_label, 'title')  # Modifiez cette ligne
+def test_title_label(book):
+    field_label = book._meta.get_field('title').verbose_name
+    assert field_label == 'title'
 
-    def test_author_max_length(self):
-        book = Book.objects.get(id=1)
-        max_length = book._meta.get_field('author').max_length
-        self.assertEqual(max_length, 50)  # Modifiez cette ligne
+def test_author_max_length(book):
+    max_length = book._meta.get_field('author').max_length
+    assert max_length == 50
 
-    def test_title_max_length(self):
-        book = Book.objects.get(id=1)
-        max_length = book._meta.get_field('title').max_length
-        self.assertEqual(max_length, 50)  # Modifiez cette ligne
+def test_title_max_length(book):
+    max_length = book._meta.get_field('title').max_length
+    assert max_length == 50
 
-    def test_object_name_is_author_title(self):
-        book = Book.objects.get(id=1)
-        expected_object_name = f'{book.author} | {book.title}'
-        self.assertEqual(expected_object_name, str(book))  # Modifiez cette ligne
+def test_object_name_is_author_title(book):
+    expected_object_name = f'{book.author} | {book.title}'
+    assert expected_object_name == str(book)
